a8455f2a876a83e338c203577ab5e51b3c11790b
Oskars Pozdnakovs <opozdnakov@gmail.com>
2017-06-20 18:49:23 +0300

    Revert "UI usability improvement"
    This reverts commit 39f08c2d4c070fa3080a641954751dc5a6bf9bb7.

--- scriptADV.js
+++ scriptADV.js
@@ -8,13 +8,12 @@ var markers = [];
 
 
 function initMap() {
-    
 
     directionsDisplay = new google.maps.DirectionsRenderer({
         draggable: true,
-        map: map,
-        panel: document.getElementById('floating-panel')
+        map: map
     });
+
     // Create a map and center it on Ilukstes iela, Riga
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 56.953748, lng: 24.195647},
@@ -28,9 +27,8 @@ function initMap() {
           optimizeWaypoints: true
     };
 
-
     directionsDisplay.addListener('directions_changed', function() {
-        document.getElementById("currDist").value = computeTotalDistance(directionsDisplay.getDirections()).toFixed(2) + " km";
+        computeTotalDistance(directionsDisplay.getDirections());
     });
 
     map.addListener('click', function(event) {
@@ -46,8 +44,12 @@ function initMap() {
         gradi = 180; //make the 1st marker be the at point straight below the middle of the circle
         markers = [];
 
-        request.waypoints = [];
+        if(!request.waypoints){
+            request.waypoints = [];
+        } else if (request.waypoints.length > 0) {
+            request.waypoints = null;
 
+        }
 
         for(i; i < 4; i++)
         {
@@ -68,7 +70,7 @@ function initMap() {
                 location: markers[i].getPosition(),
                 stopover:true
               });
-              console.log("symbian");
+              console.log(request.destination);
             }
             else{
               if(!request.waypoints){
@@ -88,7 +90,14 @@ function initMap() {
         directionsService.route(request, function(response, status) {
           if (status == google.maps.DirectionsStatus.OK) {
             directionsDisplay.setDirections(response);
-              document.getElementById("currDist").value = computeTotalDistance(response).toFixed(2) + " km";
+
+            for(i = 0; i< response.routes[0].legs.length; i++)
+            {
+              distance = distance + response.routes[0].legs[i].distance.value;
+            }
+            //alert("Kopējais garums ir "+distance/1000+" km.");
+            document.getElementById("distt").innerHTML = distance/1000;
+            $('#distt').val(distance/1000);
           } else { alert("couldn't get directions:"+status);}
         });
 
@@ -167,7 +176,8 @@ function computeTotalDistance(result) {
     for (var i = 0; i < myroute.legs.length; i++) {
         total += myroute.legs[i].distance.value;
     }
-    return total / 1000;
+    total = total / 1000;
+    document.getElementById('total').innerHTML = total + ' km';
 }
 
 // document.getElementById('distance').value
